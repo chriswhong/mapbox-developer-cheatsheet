6c1b6f95fb8858a90aae7d683be4d8d715706d7b
Chris Whong <chris.m.whong@gmail.com>
2022-11-01 23:23:17 -0400

    add virtual grid for resizing

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ function getRandomInt(min, max, type) {
 
   if (randomValue < 0) return 0;
   if (type === "x") {
-    const numColumns = calculateNumColumns();
     if (randomValue > numColumns - 1) return numColumns - 1;
   }
   return randomValue;
 }
 
-let grid = GridStack.init({
+let grid = (window.grid = GridStack.initAll({
   staticGrid: true,
   margin: 1,
-});
+  oneColumnSize: 400,
+}));
 
 const calculateNumColumns = () => {
   // add a column for every 112 px of width past 560
@@ -34,94 +34,91 @@ const calculateNumColumns = () => {
 };
 
 function resizeGrid() {
-  const numColumns = calculateNumColumns();
+  const newNumColumns = calculateNumColumns();
 
-  const cellHeight = `${100 / numColumns}vw`;
+  if (newNumColumns !== numColumns) {
+    numColumns = newNumColumns;
 
-  grid.column(numColumns, "moveScale").cellHeight(cellHeight);
+    const cellHeight = `${100 / numColumns}vw`;
 
-  // TODO: figure out how to add these listeners without a setTimeout()
-  setTimeout(() => {
-    // category click handlers
-    const categoryTiles = document.querySelectorAll(".category-tile");
-    categoryTiles.forEach((categoryTile) => {
-      categoryTile.addEventListener("click", function handleClick(event) {
-        const { category } = event.currentTarget.dataset;
+    // use the second grid to generate a new layout
 
-        var list = document.querySelectorAll(`.tile.category-${category}`);
-        const isFlipped = list[0].classList.contains(
-          "flip-card-category-flipped"
-        );
+    grid[0].column(numColumns, "none");
+    grid[1].column(numColumns, "none");
 
-        for (var i = 0; i < list.length; ++i) {
-          if (isFlipped) {
-            list[i].classList.remove("flip-card-category-flipped");
-          } else {
-            list[i].classList.add("flip-card-category-flipped");
-          }
-        }
-      });
+    layoutTiles(1, () => {
+      const layout = grid[1].save();
+      grid[0].load(layout).cellHeight(cellHeight);
+      grid[1].removeAll();
     });
-  }, 500);
+  }
 }
 
 // build a gridstack widget based on type
 const getWidget = (item) => {
   const { type, category, title, subTitle, link, iconClass, x, y } = item;
   const position = { x, y };
-  if (type === "category") {
-    return {
-      ...position,
-      minW: 2,
-      maxW: 2,
-      minH: 2,
-      maxH: 2,
-      content: `
-            <div class='wiggle-card cursor-pointer grid-stack-item-content-inner category-tile category-${category} group' data-category=${category}>
-                <img src = './img/${category}.svg'>
-                <div class='title'>${title}</div>
-                <div class='subtitle'>${subTitle}</div>
-                <div class='absolute bottom-3 invisible group-hover:visible text-xs'>Click to see tile descriptions</div>
-            </div>
-        `,
-    };
-  }
+  const id = slugify(`${category}-${title}`, { lower: true });
 
-  return {
-    ...position,
+  let content = `
+  <div class="flip-card grid-stack-item-content-inner tile category-${category}">
+    <div class="flip-card-inner">
+      <div class="flip-card-front category-${category}">
+          ${title}
+          <div class='absolute left-2 bottom-4 text-xs flex items-center'>
+          ${iconClass ? `<i class="fa-lg ${iconClass}"></i>` : ""}
+          </div>
+      </div>
+      <div class="flip-card-back category-${category}">
+        <a href = '${link}' target='_blank' noopener noreferrer>
+          <div class="flip-card-back-inner">
+              ${subTitle}
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+`;
+
+  let size = {
     minW: 1,
     maxW: 1,
     minH: 1,
     maxH: 1,
-    content: `
-        <div class="flip-card grid-stack-item-content-inner tile category-${category}">
-          <div class="flip-card-inner">
-            <div class="flip-card-front category-${category}">
-                ${title}
-                <div class='absolute left-2 bottom-4 text-xs flex items-center'>
-                ${iconClass ? `<i class="fa-lg ${iconClass}"></i>` : ""}
-                </div>
-            </div>
-            <div class="flip-card-back category-${category}">
-              <a href = '${link}' target='_blank' noopener noreferrer>
-                <div class="flip-card-back-inner">
-                    ${subTitle}
-                </div>
-              </a>
-            </div>
+  };
+
+  if (type === "category") {
+    content = `
+          <div class='wiggle-card cursor-pointer grid-stack-item-content-inner category-tile category-${category} group' data-category=${category}>
+              <img src = './img/${category}.svg'>
+              <div class='title'>${title}</div>
+              <div class='subtitle'>${subTitle}</div>
+              <div class='absolute bottom-3 invisible group-hover:visible text-xs'>Click to see tile descriptions</div>
           </div>
-        </div>
-      `,
+      `;
+
+    size = {
+      minW: 2,
+      maxW: 2,
+      minH: 2,
+      maxH: 2,
+    };
+  }
+
+  return {
+    id,
+    ...position,
+    ...size,
+    content,
   };
 };
 
 // scan the grid to find the first available 2x2 position
-const getFreeNearbyPositionForCategory = () => {
+const getFreeNearbyPositionForCategory = (gridId) => {
   let x = 0;
   let y = 0;
-  const numColumns = calculateNumColumns();
   while (true) {
-    const isEmpty = grid.isAreaEmpty(x, y, 2, 2);
+    const isEmpty = grid[gridId].isAreaEmpty(x, y, 2, 2);
 
     if (isEmpty) {
       return [x, y];
@@ -137,9 +134,8 @@ const getFreeNearbyPositionForCategory = () => {
 // find a nearby 1x1 adjacent to this tile's category tile
 // if one cannot be found immediately adjacent to the category tile,
 // go one row/column further out
-const getFreeNearbyPosition = (categoryTilePosition, category) => {
+const getFreeNearbyPosition = (categoryTilePosition, gridId) => {
   let attempts = 0;
-  let foundEmptyPosition = false;
   let depth = 1;
 
   const [categoryTileX, categoryTileY] = categoryTilePosition;
@@ -153,7 +149,7 @@ const getFreeNearbyPosition = (categoryTilePosition, category) => {
       depth += 1;
     }
 
-    const isEmpty = grid.isAreaEmpty(x, y, 1, 1);
+    const isEmpty = grid[gridId].isAreaEmpty(x, y, 1, 1);
 
     if (isEmpty) {
       return [x, y];
@@ -163,7 +159,7 @@ const getFreeNearbyPosition = (categoryTilePosition, category) => {
   }
 };
 
-const layoutTiles = () => {
+const layoutTiles = (gridId, cb) => {
   var categoryTilePositions = {};
 
   var i = 0;
@@ -175,16 +171,16 @@ const layoutTiles = () => {
       const item = mapboxItems[i];
 
       if (item.type === "category") {
-        [x, y] = getFreeNearbyPositionForCategory();
+        [x, y] = getFreeNearbyPositionForCategory(gridId);
         categoryTilePositions[item.category] = [x, y];
       } else {
         [x, y] = getFreeNearbyPosition(
           categoryTilePositions[item.category],
-          item.category
+          gridId
         );
       }
 
-      grid.addWidget(
+      grid[gridId].addWidget(
         getWidget({
           ...item,
           x,
@@ -194,6 +190,8 @@ const layoutTiles = () => {
       i++;
       if (i < mapboxItems.length) {
         placeTile();
+      } else {
+        cb();
       }
     }, 0);
   }
@@ -201,9 +199,47 @@ const layoutTiles = () => {
   placeTile();
 };
 
-layoutTiles();
-resizeGrid(); // finally size to actual window
+let numColumns = calculateNumColumns();
+grid[0].column(numColumns, "none");
 
-window.addEventListener("resize", function () {
-  resizeGrid();
+layoutTiles(0, () => {
+  // category click handlers
+  const categoryTiles = document.querySelectorAll(".category-tile");
+  categoryTiles.forEach((categoryTile) => {
+    categoryTile.addEventListener("click", function handleClick(event) {
+      const { category } = event.currentTarget.dataset;
+
+      var list = document.querySelectorAll(`.tile.category-${category}`);
+      const isFlipped = list[0].classList.contains(
+        "flip-card-category-flipped"
+      );
+
+      for (var i = 0; i < list.length; ++i) {
+        if (isFlipped) {
+          list[i].classList.add("flip-card-category-default");
+          list[i].classList.remove("flip-card-category-flipped");
+        } else {
+          list[i].classList.remove("flip-card-category-default");
+          list[i].classList.add("flip-card-category-flipped");
+        }
+      }
+    });
+  });
 });
+
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+window.addEventListener(
+  "resize",
+  debounce(function () {
+    resizeGrid();
+  })
+);
